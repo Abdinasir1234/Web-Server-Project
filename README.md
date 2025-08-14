ef94116a20fa00254e569d2ab43d3b3f5c348c7f
Abdinasir <112059604+Abdinasir1234@users.noreply.github.com>
2025-08-14 18:50:58 +0000

    testing out app.get and what you can do when sending. installed ejs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Web-Server-Project
 This a repo where i will make my very own web server. This is for me to learn more about webserver  as a student.
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-
+app.set('view engine','ejs');   
 
 app.get('/',(req,res)=>{
     console.log("hello");
-    res.status(404);
+
+    res.render('index',{text:"world"}) 
 })
 
 app.listen(3000);
